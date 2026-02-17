d0179b1a326da57a8f29272e0101fb0d4b99428e
Louis <louisavon77@gmail.com>
2026-02-17 14:44:38 +0100

    creat : task01

--- task00.py
+++ task00.py
@@ -4,8 +4,8 @@ def load_json(filepath: str) -> dict:
     if not isinstance(filepath, str):
         raise TypeError
 
-    with open(filepath, "r", encoding="utf-8") as f:
+    with open(filepath, "r", encoding="utf-8") as f: #liiii le fichier
         content = f.read()
 
-    print(content[:100])
+    print(content[:100]) #uniquement les 1OO premiers caractères
     return json.loads(content)
---
+++ task01.py
@@ -0,0 +1,32 @@
+from task00 import load_json
+
+def display_info() -> None:
+    data = load_json("./nobels.json")
+
+    categories = set() #set stocker les catégories sans doublons
+    names = set()
+    countries = set()
+
+    for laureate in data.get("laureates", []):
+
+        firstname = laureate.get("firstname", "") or "" #récupère le prénom
+        surname = laureate.get("surname", "") or ""
+        full_name = f"{firstname} {surname}".strip()
+        if full_name:
+            names.add(full_name)
+
+        code = laureate.get("bornCountryCode", "") or ""
+        country = laureate.get("bornCountry", "") or ""
+        code = code.strip()
+        country = country.strip()
+        if code and country:
+            countries.add((code, country))
+
+        for prize in laureate.get("prizes", []):
+            category = (prize.get("category", "") or "").strip()
+            if category:
+                categories.add(category)
+
+    print(sorted(categories))
+    print(sorted(names))
+    print(sorted(countries))
